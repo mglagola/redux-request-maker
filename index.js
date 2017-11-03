c5548f98d862760c77aafd6e97a8d545df34a919
Mark Glagola <mark.glagola@gmail.com>
2017-11-02 23:22:36 -0500

    initial v

---
+++ index.js
@@ -0,0 +1,7 @@
+const apiMiddleware = require('./lib/api-middleware');
+const createReduxRequest = require('./lib/create-redux-request');
+
+module.exports = {
+    apiMiddleware,
+    createReduxRequest,
+};

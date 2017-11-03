090d848f51e1e9280a88f9273f83f9d771221678
Mark Glagola <mark.glagola@gmail.com>
2017-11-02 23:27:17 -0500

    renaming

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const apiMiddleware = require('./lib/api-middleware');
+const middleware = require('./lib/middleware');
 const createReduxRequest = require('./lib/create-redux-request');
 
 module.exports = {
-    apiMiddleware,
+    middleware,
     createReduxRequest,
 };

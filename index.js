26f1738f11082291baf683e821e224edc0226108
Mark Glagola <mark.glagola@gmail.com>
2017-11-20 13:22:04 -0600

    added create-redux-coll-request.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const middleware = require('./lib/middleware');
 const createReduxRequest = require('./lib/create-redux-request');
+const createReduxCollRequest = require('./lib/create-redux-coll-request');
 
 module.exports = {
     middleware,
     createReduxRequest,
+    createReduxCollRequest,
 };
